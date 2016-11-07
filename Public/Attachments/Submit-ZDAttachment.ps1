f20f17ecbf9f367853edcc6c656d21845e0cfbe4
MSAdministrator <rickardja@live.com>
2016-11-07 09:18:21 -0500

    Added GUI prompt for ticket number and changed Get-ChildItem with Test-Path

--- Public/Attachments/Submit-ZDAttachment.ps1
+++ Public/Attachments/Submit-ZDAttachment.ps1
@@ -20,44 +20,107 @@ function Submit-ZDAttachment
 
     Begin
     {
-        $Result = @{}
+        $Result = @()
+        $firsttoken = ""
     }
     Process
     {
-        foreach ($item in $Attachment)
+        for ($i = 0; $i -lt $Attachment.Count; $i++)
         {
             $tempobj = @{}
-            $params = @{
-                Uri = "https://$Domain.zendesk.com/api/v2/uploads.json?filename=$(Split-Path -Leaf $item)&token=$firsttoken"
-                Method = 'Post'
-                Headers = $Headers
-                ContentType = 'application/binary'
-                InFile = $item
-            }
-           
-            try
-            {
-                $AttObject = Invoke-RestMethod @params
+            $params = @{}
 
-                $tempobj.Token = $AttObject.upload.token
-                $tempobj.Expires = $AttObject.upload.expires_at
-                $tempobj.URL = $AttObject.upload.attachments.url
-                $tempobj.ID = $AttObject.upload.attachments.id
-                $tempobj.FileName = $AttObject.upload.attachments.file_name
-                $tempobj.ContentType = $AttObject.upload.attachments.content_type
-                $tempobj.Size = $AttObject.upload.attachments.size
-            }
-            catch
+            if (0 -eq $i)
             {
-                Write-Warning -Message "Error uploading attachment to ZenDesk: $($Error | select -Property *)"
+                $params = @{
+                    Uri = "https://$Domain.zendesk.com/api/v2/uploads.json?filename=$(Split-Path -Leaf $Attachment[$i])"
+                    Method = 'Post'
+                    Headers = $Headers
+                    ContentType = 'application/binary'
+                    InFile = $Attachment[$i]
+                }
+
+                try
+                {
+                    $AttObject = @()
+
+                    $AttObject = Invoke-RestMethod @params
+
+                #    $tempProp = @{
+                        $TempObject.Token = $AttObject.upload.token
+                        $TempObject.Expires = $AttObject.upload.expires_at
+                        $TempObject.URL = $AttObject.upload.attachments.url
+                        $TempObject.ID = $AttObject.upload.attachments.id
+                        $TempObject.FileName = $AttObject.upload.attachments.file_name
+                        $TempObject.ContentType = $AttObject.upload.attachments.content_type
+                        $TempObject.Size = $AttObject.upload.attachments.size
+                 #   }
+
+                  #  $TempObject = New-Object -TypeName PSCustomObject -Property $tempProp
+                }
+                catch
+                {
+                    Write-Warning -Message "Error uploading attachment to ZenDesk: $($Error | select -Property *)"
+                }
+
+                Write-Debug "TempObject[$i] = $($TempObject | Out-String)"
+
+                $firsttoken = $TempObject.Token
+
+                Write-Debug "FirstToken[$i] = $firsttoken"
+
+                $Result += $TempObject
             }
 
-            $Result += $tempobj
+            if ($i -gt 0)
+            {
+
+                Write-Host "firsttoken[$i] = $firsttoken"
+
+                $tempobj = @{}
+
+                $params = @{
+                    Uri = "https://$Domain.zendesk.com/api/v2/uploads.json?filename=$(Split-Path -Leaf $Attachment[$i])&token=$firsttoken"
+                    Method = 'Post'
+                    Headers = $Headers
+                    ContentType = 'application/binary'
+                    InFile = $Attachment[$i]
+                }
+
+
+
+                try
+                {
+                    $AttObject = @()
+                    $AttObject = Invoke-RestMethod @params
+
+                    #$tempProp = @{
+                        $TempObject.Token = $AttObject.upload.token
+                        $TempObject.Expires = $AttObject.upload.expires_at
+                        $TempObject.URL = $AttObject.upload.attachments.url
+                        $TempObject.ID = $AttObject.upload.attachments.id
+                        $TempObject.FileName = $AttObject.upload.attachments.file_name
+                        $TempObject.ContentType = $AttObject.upload.attachments.content_type
+                        $TempObject.Size = $AttObject.upload.attachments.size
+                    #}
+
+                    #$TempObject = New-Object -TypeName PSCustomObject -Property $tempProp
+                }
+                catch
+                {
+                    Write-Warning -Message "Error uploading attachment to ZenDesk: $($Error | select -Property *)"
+                }
+
+                Write-Debug "TempObject[$i] = $($TempObject | Out-String)"
+
+                $Result += $TempObject
+                
+            }
         }
-        $firsttoken = $tempobj.Token
     }
     End
     {
+        
         Add-ObjectDetail -InputObject $Result -TypeName PoshZD.Attachment -DefaultProperties Token,FileName,Expires
     }
 }
