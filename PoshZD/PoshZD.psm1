3d2e59f56580404339431715fc882c4e5f4cab6f
MSAdministrator <MSAdministrator@users.noreply.github.com>
2018-11-29 13:16:35 -0600

    Removed configuration import to module

--- PoshZD/PoshZD.psm1
+++ PoshZD/PoshZD.psm1
@@ -1,37 +1,4 @@
-﻿if (!(Test-Path "$env:APPDATA\PoshZD\PoshZD-Configuration.psd1"))
-{
-    try
-    {
-        New-Item -Path "$env:APPDATA\PoshZD" -ItemType Directory
-    }
-    catch
-    {
-        Write-Error -Message 'Unable to create configuration folder'
-    }
-
-# Create a PSD1 file
-@"
-@{
-
-    # CHANGE THESE SETTINGS BEFORE IMPORTING MODULE
-
-    Domain = 'Contoso'
-    Email = 'first.last@contoso.com'
-    Token = 'ZD_TOKEN_STRING'
-}
-
-"@ | Out-File -FilePath "$ENV:APPDATA\PoshZD\PoshZD-Configuration.psd1"
-
-}
-# Read the file
-$File = Import-LocalizedData -BaseDirectory "$ENV:APPDATA\PoshZd\" -FileName "PoshZD-Configuration.psd1"
-
-Set-Variable -Name Domain -Value $File.Domain -Scope Global
-
-Set-Variable -Name Headers -Value @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($File.Email)/token:$($File.Token)"));} -Scope Global
-
-
-#requires -Version 2
+﻿#requires -Version 2
 #Get public and private function definition files.
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue
               Get-ChildItem -Path $PSScriptRoot\Public\*\*.ps1 -Recurse -ErrorAction SilentlyContinue )
